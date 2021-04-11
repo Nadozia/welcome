6eedd4608325c70ba37a2d623ece11a62c4a6323
Lewix Wong <qpmz9713@gmail.com>
2021-04-11 17:28:33 +0800

    fix project box background toggle

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
-const image1 = document.getElementById('image1');
-const image2 = document.getElementById('image2');
-const image3 = document.getElementById('image3');
-const projectBox = document.getElementById('project-box');
+const projectBoxs = document.querySelectorAll('.project-box')
 
-// Dark or Light Images
-function imageMode(color){
-    image1.src =`img/undraw_proud_coder_${color}.svg`;
-    image2.src =`img/undraw_feeling_proud_${color}.svg`;
-    image3.src =`img/undraw_conceptual_idea_${color}.svg`;
-}
 
 function toggleDarkLightMode(isDark){
     nav.style.background = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
-    projectBox.style.background = isDark ? 'rgb(255 255 255 / 50%)' :'rgb(0 0 0 / 50%)';
+    projectBoxs[0].style.background = isDark ? 'rgb(255 255 255 / 50%)' :'rgb(0 0 0 / 50%)';
+    projectBoxs[1].style.background = isDark ? 'rgb(255 255 255 / 50%)' :'rgb(0 0 0 / 50%)';
+    projectBoxs[2].style.background = isDark ? 'rgb(255 255 255 / 50%)' :'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = isDark ? 'Dark Mode' :'Light Mode';
     isDark ?toggleIcon.children[1].classList.replace('fa-sun','fa-moon'):
     toggleIcon.children[1].classList.replace('fa-moon','fa-sun');
-    isDark ? imageMode('Dark') :imageMode('light');   
-}
+    }
 
 // Switch Theme Dynamically
 function switchTheme(event){
